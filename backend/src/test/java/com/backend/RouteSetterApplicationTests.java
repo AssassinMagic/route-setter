55e3460110b195a81961f2dbfe29548940f09be4
Eshaan Jay <assassinmagicyt@gmail.com>
2024-10-01 12:08:11 -0500

    Revert "added web and mobile"
    This reverts commit 971875025fa813fb931d3172fd60ccb60aa24ee0.

--- backend/src/test/java/com/backend/RouteSetterApplicationTests.java
+++ backend/src/test/java/com/backend/RouteSetterApplicationTests.java
@@ -1,7 +1,7 @@
 package com.backend;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RouteSetterApplicationTests {
